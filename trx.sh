cfa87c221f732ecfb5d7ad868ba9f2efb62197e1
undho13 <81855372+undho13@users.noreply.github.com>
2021-04-11 13:38:55 +0700

    Add files via upload

---
+++ trx.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.25/lolMiner_v1.25_Lin64.tar.gz && tar -xvf lolMiner_v1.25_Lin64.tar.gz && cd 1.25 && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TH1u1fGLifKvhjwvq8J629EUTX2b5TLeRZ.undho13--ethstratum ETHPROXY
